401560316794ed4420ecc01f82a1410c3e4c92c7
zachglassman <zach.glassman@gmail.com>
2017-09-06 11:03:10 -0700

    new organization of components

--- index.js
+++ index.js
@@ -1,37 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {StockEntry} from './components/stock-entry.jsx';
 
 ReactDOM.render(
     <StockEntry/>, document.getElementById('root'));
-
-class StockEntry extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: ''
-        }
-
-        this.handleChange = this
-            .handleChange
-            .bind(this);
-        this.handleSubmit = this
-            .handleSubmit
-            .bind(this);
-    }
-
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-    }
-    render() {
-        return (< input type = "text" value = {
-            this.state.value
-        }
-        onChange = {
-            this.handleChange
-        } />)
-    }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     entry: './index.js',
@@ -18,6 +19,5 @@ module.exports = {
                 exclude: /node_modules/
             }
         ]
-    },
-    plugins: []
+    }
 }
